ab123d664faef92dcbaa51979207405f10b31f40
Keri Gagnow <166183357+KeriGagnow@users.noreply.github.com>
2026-06-23 18:01:01 -0500

    Initial Commit

---
+++ Happy HabitsTests/Happy_HabitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_HabitsTests.swift
+//  Happy HabitsTests
+//
+//  Created by Keri on 6/23/26.
+//
+
+import Testing
+@testable import Happy_Habits
+
+struct Happy_HabitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
